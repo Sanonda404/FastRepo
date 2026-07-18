316e48ccebec9e11b8d623b10b79383e0edcf5e4
Coder-85 <fiskwilson34@gmail.com>
2026-07-19 00:57:53 +0600

    Update team members DDL
    Store collaborator_id instead of user_id.

--- backend/src/models/team_members.py
+++ backend/src/models/team_members.py
@@ -3,7 +3,7 @@ import asyncpg
 TEAM_MEMBERS_TABLE_DDL = """
 CREATE TABLE IF NOT EXISTS team_members (
     team_id INT NOT NULL REFERENCES teams(id) ON DELETE CASCADE,
-    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE
+    collaborator_id INT NOT NULL REFERENCES repository_collaborators(id) ON DELETE CASCADE
 )
 """
 
