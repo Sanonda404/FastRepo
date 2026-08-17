304cd7230124021acdd31fc96688b3329314a435
Coder-85 <fiskwilson34@gmail.com>
2026-08-17 12:28:08 +0600

    Use newer vite recommendation

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(import.meta.dirname, "./src"),
     },
   },
   server: {
     proxy: {
-      // Proxies any request starting with /api to your FastAPI backend
       "/api": {
         target: "http://localhost:8000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""), // Removes '/api' prefix before sending to FastAPI
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
-  }
-})
+  },
+});
