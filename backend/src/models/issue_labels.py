a51417e3bb88a933ee704360aed70e451a6d86b2
Sanonda404 <sanondadeb@gmail.com>
2026-08-21 12:07:13 +0600

    fixed errors

--- backend/src/models/issue_labels.py
+++ backend/src/models/issue_labels.py
@@ -3,7 +3,7 @@ import asyncpg
 ISSUE_LABELS_TABLE_DDL = """
 CREATE TABLE IF NOT EXISTS issue_assignees (
     issue_id INT NOT NULL REFERENCES issues(id) ON DELETE CASCADE,
-    label_id INT NOT NULL REFERENCES labels(id) ON DELETE CASCADE
+    label_id INT NOT NULL REFERENCES labels(id) ON DELETE CASCADE,
     CONSTRAINT issue_assignees_pkey PRIMARY KEY (issue_id, assignee_id)
 )
 """
--- backend/src/models/issue_pull_requests.py
+++ backend/src/models/issue_pull_requests.py
@@ -3,7 +3,7 @@ import asyncpg
 ISSUE_PULL_REQUESTS_TABLE_DDL = """
 CREATE TABLE IF NOT EXISTS issue_pull_requests (
     issue_id INT NOT NULL REFERENCES issues(id) ON DELETE CASCADE,
-    pull_request_id INT NOT NULL REFERENCES pull_requests(id) ON DELETE CASCADE
+    pull_request_id INT NOT NULL REFERENCES pull_requests(id) ON DELETE CASCADE,
     CONSTRAINT issue_pull_requests_pkey PRIMARY KEY (issue_id, pull_request_id)
 )
 """
