33b00300af5074d8657fb1a0ce2cdb5525022dd8
Sanonda404 <sanondadeb@gmail.com>
2026-08-26 10:05:21 +0600

    added role restrictions

--- backend/src/models/repository_collaborators.py
+++ backend/src/models/repository_collaborators.py
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS repository_collaborators (
     id SERIAL PRIMARY KEY,
     repository_id INT NOT NULL REFERENCES repositories(id) ON DELETE CASCADE,
     user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    role VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL CHECK (role IN ('Admin', 'Maintainer', 'Member', 'Viewer')),
     CONSTRAINT unique_repo_collaborator UNIQUE (repository_id, user_id)
 );
 """
